c4e048e8ac2aed090940c3fb11d3ebbf01462c99
alopezp-dev <alopezia@alumnos.unex.es>
2025-10-04 18:20:16 +0200

    Merge pagina

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>📚EbauX V0.0📚</h1>
+      <h1>📚EbauX V0.0 - MERGE📚</h1>
     </div>
   );
 }
